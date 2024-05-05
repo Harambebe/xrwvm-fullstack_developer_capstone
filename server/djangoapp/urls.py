6e1bf979b0462242b477bb1fc39b0b0c2934f733
Harambebe <harambe.just.a.gorilla@gmail.com>
2024-05-04 23:24:18 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     path(route='login', view=views.login_user, name='login'),
     path('logout/', views.logout_request, name='logout'),
-
+    path('register/', views.registration, name='register'),
     # path for dealer reviews view
 
     # path for add a review view
